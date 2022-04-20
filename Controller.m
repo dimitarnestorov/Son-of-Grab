75256f0dc2d04608cd9e1f37f84b4bb785d1d945
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-04-21 02:24:14 +0300

    Update license

--- Controller.m
+++ Controller.m
@@ -1,11 +1,3 @@
-/*
- Copyright (C) 2015 Apple Inc. All Rights Reserved.
- See LICENSE.txt for this sample’s licensing information
- 
- Abstract:
- Handles UI interaction and retrieves window images.
- */
-
 #import "Controller.h"
 
 @interface WindowListApplierData : NSObject
